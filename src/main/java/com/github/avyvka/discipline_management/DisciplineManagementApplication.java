11308a893b013ed14a63f1a61919ee2f0944197d
Avyvka <lamonov.yura@gmail.com>
2025-03-25 07:06:59 +0300

    Initial commit

---
+++ src/main/java/com/github/avyvka/discipline_management/DisciplineManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.avyvka.discipline_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplineManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplineManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/avyvka/discipline_management/DisciplineManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.avyvka.discipline_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplineManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
