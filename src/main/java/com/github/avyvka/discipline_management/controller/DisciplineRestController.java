1d7ee8bba7fadd2c480f694d848ed64744e77287
Avyvka <lamonov.yura@gmail.com>
2025-03-27 03:30:45 +0300

    feat: Добавление реализации RestController под каждую entity

---
+++ src/main/java/com/github/avyvka/discipline_management/controller/DisciplineRestController.java
@@ -0,0 +1,18 @@
+package com.github.avyvka.discipline_management.controller;
+
+import com.github.avyvka.discipline_management.controller.support.AbstractCrudController;
+import com.github.avyvka.discipline_management.model.dto.DisciplineDto;
+import com.github.avyvka.discipline_management.service.support.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/disciplines")
+public class DisciplineRestController extends AbstractCrudController<DisciplineDto, String> {
+
+    @Autowired
+    public DisciplineRestController(CrudService<DisciplineDto, String> service) {
+        super(service);
+    }
+}
---
+++ src/main/java/com/github/avyvka/discipline_management/controller/StudentRestController.java
@@ -0,0 +1,18 @@
+package com.github.avyvka.discipline_management.controller;
+
+import com.github.avyvka.discipline_management.controller.support.AbstractCrudController;
+import com.github.avyvka.discipline_management.model.dto.StudentDto;
+import com.github.avyvka.discipline_management.service.support.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentRestController extends AbstractCrudController<StudentDto, String> {
+
+    @Autowired
+    public StudentRestController(CrudService<StudentDto, String> service) {
+        super(service);
+    }
+}
